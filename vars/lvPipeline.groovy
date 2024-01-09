16bde9d1c3ca686b09b9c74c1fdfc4d36ec5338b
LINSYS TECHNOLOGIES <jlinisa@linsys-technologies.fr>
2024-01-09 15:56:59 +0100

    ajout case 21.0

--- vars/lvPipeline.groovy
+++ vars/lvPipeline.groovy
@@ -15,6 +15,9 @@ def call(lvProjectPath, lvBuildSpecName, lvVersion, lvBitness) {
 	  case "20.0":
 		lvVersion="2020"
 		break
+      case "21.0":
+		lvVersion="2021"
+		break
 	}
 
 	node {
@@ -68,7 +71,7 @@ def call(lvProjectPath, lvBuildSpecName, lvVersion, lvBitness) {
 				}
 		}
 		
-	
+
 	}
 }
 
